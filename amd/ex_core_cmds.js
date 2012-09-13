fa86d9141def8f67acdf096bf7104195444650d3
Cyan Ogilvie <cyan@Cyans-MacBook-Air.local>
2012-09-13 15:10:20 +0200

    Added expr and incr, factored out control structure commands

--- amd/ex_core_cmds.js
+++ amd/ex_core_cmds.js
@@ -2,13 +2,17 @@
 /*global define */
 
 define([
+	'./ex_control_cmds',
 	'./ex_list_cmds',
 	'./ex_dict_cmds',
-	'./types'
+	'./types',
+	'./list'
 ], function(
+	ex_control_cmds,
 	ex_list_cmds,
 	ex_dict_cmds,
-	types
+	types,
+	tcllist
 ){
 'use strict';
 
@@ -18,13 +22,13 @@ function install(interp) {
 	if (interp.register_extension('ex_core_cmds')) {return;}
 
 	/* Core commands still to implement:
-	 subst time eval proc throw break variable error catch clock info array if
-	 coroutine global switch update for append format package namespace binary
-	 scan apply trace zlib while after vwait continue uplevel try foreach
-	 rename regexp upvar tailcall expr unset regsub interp incr string yield
+	 subst time eval proc throw break variable error catch clock info array
+	 coroutine global update append format package namespace binary scan apply
+	 trace zlib after vwait continue uplevel rename regexp upvar tailcall
+	 unset regsub interp incr string yield
 	 */
 
-	interp.registerCommand('set', function(args, interp){
+	interp.registerCommand('set', function(args){
 		interp.checkArgs(args, [1, 2], 'varName ?newValue?');
 		if (args.length === 2) {
 			return interp.get_var(args[1]);
@@ -32,7 +36,28 @@ function install(interp) {
 		return interp.set_var(args[1], args[2]);
 	});
 
-	interp.registerCommand('return', function(args, interp){
+	interp.registerCommand('expr', function(args){
+		interp.checkArgs(args, [1, null], 'arg ?arg ...?');
+		if (args.length === 2) {
+			return interp.TclExpr(args[1]);
+		}
+		var i, str_args = [];
+		for (i=1; i<args.length; i++) {
+			str_args.push(args.toString());
+		}
+		return interp.TclExpr(str_args.join(' '));
+	});
+
+	interp.registerCommand('incr', function(args){
+		interp.checkArgs(args, [1, 2], 'varname ?increment?');
+		var intobj = interp.get_var(args[1], true),
+			increment = args[2] === undefined ? 1 : args[2].GetInt();
+		intobj.jsval = intobj.GetInt() + increment;
+		intobj.bytes = null;
+		return intobj;
+	});
+
+	interp.registerCommand('return', function(args){
 		interp.checkArgs(args, [0, 1], '?value?');
 		if (args.length === 2) {
 			return new TclResult(types.RETURN, interp.get_var(args[1]));
@@ -40,6 +65,7 @@ function install(interp) {
 		return new TclResult(types.RETURN, '');
 	});
 
+	ex_control_cmds.install(interp);
 	ex_list_cmds.install(interp);
 	ex_dict_cmds.install(interp);
 }
